62b8aae5399f77e267c0620df6b2e89ca26c61af
rhsftc <ronsearle@hotmail.com>
2026-05-07 16:18:42 -0500

    Refactor Claw subsystem to use final modifier for servo instance and remove redundant clawServo declaration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextSubSystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextSubSystems/Claw.java
@@ -7,12 +7,11 @@ import dev.nextftc.hardware.positionable.SetPosition;
 
 public class Claw implements Subsystem {
     public static final Claw INSTANCE = new Claw();
-    private ServoEx servo = new ServoEx("clawservo");
+    private final ServoEx servo = new ServoEx("clawservo");
 
     private Claw() {
     }
 
-    private ServoEx clawServo = new ServoEx("clawServo");
     public Command open = new SetPosition(servo, 0.1).requires(this);
     public Command close = new SetPosition(servo, 0.2).requires(this);
 }
