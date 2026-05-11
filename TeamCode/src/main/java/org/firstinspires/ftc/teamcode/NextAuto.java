ae959559d32c0e057159fc3fb69a63ec49695552
rhsftc <ronsearle@hotmail.com>
2026-05-11 13:57:52 -0500

    Add autonomous routine to NextAuto for coordinated Claw and Lift actions using NextFTC.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextAuto.java
@@ -3,7 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.NextSubSystems.Claw;
+import org.firstinspires.ftc.teamcode.NextSubSystems.Lift;
 
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 
@@ -13,5 +18,25 @@ public class NextAuto extends NextFTCOpMode {
         addComponents(
                 new SubsystemComponent(Claw.INSTANCE, Claw.INSTANCE)
         );
+    }
+
+    private Command autonomousRoutine() {
+        return new SequentialGroup(
+                Lift.INSTANCE.toHigh,
+                new ParallelGroup(
+                        Lift.INSTANCE.toMiddle,
+                        Claw.INSTANCE.close
+                ),
+                new Delay(0.5),
+                new ParallelGroup(
+                        Claw.INSTANCE.open,
+                        Lift.INSTANCE.toLow
+                )
+        );
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        autonomousRoutine().schedule();
     }
 }
