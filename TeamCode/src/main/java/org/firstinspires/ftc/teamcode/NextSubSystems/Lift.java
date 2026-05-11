71a8cd7be5abd4430e4127b72a4f061bf7f4e1ee
rhsftc <ronsearle@hotmail.com>
2026-05-11 13:50:46 -0500

    Add Lift subsystem with motor control and position commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextSubSystems/Lift.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.NextSubSystems;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+import dev.nextftc.hardware.powerable.SetPower;
+
+public class Lift implements Subsystem {
+    public static final Lift INSTANCE = new Lift();
+
+    private Lift() {
+    }
+
+    private MotorEx motor = new MotorEx("motor");
+
+    private ControlSystem controlSystem = ControlSystem.builder()
+            .posPid(0.005, 0, 0)
+            .elevatorFF(0)
+            .build();
+
+    public Command toLow = new RunToPosition(controlSystem, 0).requires(this);
+    public Command toMiddle = new RunToPosition(controlSystem, 500).requires(this);
+    public Command toHigh = new RunToPosition(controlSystem, 1200).requires(this);
+
+    @Override
+    public void periodic() {
+        motor.setPower(controlSystem.calculate(motor.getState()));
+    }
+}
