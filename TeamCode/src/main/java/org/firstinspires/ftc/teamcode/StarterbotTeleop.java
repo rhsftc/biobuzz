930d9c654318f9a407c91e36a5fe75e1924a9ba8
rhsftc <ronsearle@hotmail.com>
2026-09-15 13:55:14 -0500

    Remove unused imports from StarterbotTeleop.java to clean up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterbotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterbotTeleop.java
@@ -23,15 +23,8 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 
 /*
