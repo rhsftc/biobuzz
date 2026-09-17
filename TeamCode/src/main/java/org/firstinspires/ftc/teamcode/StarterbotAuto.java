bb7edb6b4534bf79baf23bd0c7947691f4362421
rhsftc <ronsearle@hotmail.com>
2026-09-17 14:05:42 -0500

    Add timer functionality to StarterbotAuto for controlled delay during autonomous mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterbotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterbotAuto.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Starterbot Auto", group = "Auto", preselectTeleOp = "StarterBot Teleop")
 public class StarterbotAuto extends OpMode {
 
     RobotHardware robot = new RobotHardware(this);
     private boolean runAuto = true;
+    private ElapsedTime timer = new ElapsedTime();
 
     @Override
     public void init() {
@@ -27,6 +29,10 @@ public class StarterbotAuto extends OpMode {
     public void loop() {
         if (runAuto) {
             robot.arcadeDrive(.5, 0);
+            timer.reset();
+            // Wait for half a second
+            while (timer.seconds() < .5) {
+            }
             robot.arcadeDrive(0, 0);
         }
     }
